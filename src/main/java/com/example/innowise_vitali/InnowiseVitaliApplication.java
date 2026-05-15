dab49351e48459921968606a7ee943fd2504981a
Vitali Artyomchik <pdijsj2992@gmail.com>
2026-05-15 13:48:12 +0300

    initial commit

---
+++ src/main/java/com/example/innowise_vitali/InnowiseVitaliApplication.java
@@ -0,0 +1,13 @@
+package com.example.innowise_vitali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseVitaliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnowiseVitaliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/innowise_vitali/InnowiseVitaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.innowise_vitali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnowiseVitaliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
